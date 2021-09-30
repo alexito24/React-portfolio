c5f8dc664c6fde92519acc6aaaeb4eea9174db2e
Heriberto lopez <alexito2002.ll@gmail.com>
2021-09-29 20:01:43 -0600

    added navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 export const Navigation = () => {
+  const styles = {
+    nav: {
+      display: "flex",
+      justifyContent: "flex-end",
+      backgroundColor: "#0a343b",
+      padding: "14px 20px",
+      textDecoration: "none",
+      textAlign: "center",
+      color: "rgb(83, 235, 83)",
+    },
+  };
   return (
-    <>
-      <a href="#top">About me </a>
-      <a href="#work">Work </a>
-      <a href="#contact">Contact me </a>
-      <a href="#bottom">Resume</a>
-    </>
+    <div>
+      <a href="#top" style={styles.nav}>
+        About me{" "}
+      </a>
+      <a href="#work" style={styles.nav}>
+        Work{" "}
+      </a>
+      <a href="#contact" style={styles.nav}>
+        Contact me{" "}
+      </a>
+      <a href="#bottom" style={styles.nav}>
+        Resume
+      </a>
+    </div>
   );
 };

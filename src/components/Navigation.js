5aae15f743f0d2e80020fe608257a257e2d3dd29
Heriberto lopez <alexito2002.ll@gmail.com>
2021-09-29 19:45:17 -0600

    added navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const Navigation = () => {
-  return <div></div>;
+  return (
+    <>
+      <a href="#top">About me </a>
+      <a href="#work">Work </a>
+      <a href="#contact">Contact me </a>
+      <a href="#bottom">Resume</a>
+    </>
+  );
 };

c506160a97e6d84bbdb29d4eeb48e82690465865
Heriberto lopez <alexito2002.ll@gmail.com>
2021-09-29 19:40:54 -0600

    added hearder

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={
-            "https://www.helpguide.org/wp-content/uploads/table-with-grains-vegetables-fruit-768.jpg"
-          }
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <h1>Alex Lopez</h1>
+      <img
+        src="https://ecrcommunity.plos.org/wp-content/uploads/sites/9/legacy-featured-images/programming-1009134_1920-16x9.jpg"
+        atl="computer coding"
+        width="100%"
+        height="200px"
+      ></img>
+    </>
+  );
+};
